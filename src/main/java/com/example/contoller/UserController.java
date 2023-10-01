a560758d9227546de8e41aa885148f9b1772edba
Oleh Kulbaba <47332977+BranetE@users.noreply.github.com>
2023-10-01 19:48:01 +0300

    Updated UserController

--- src/main/java/com/example/contoller/UserController.java
+++ src/main/java/com/example/contoller/UserController.java
@@ -23,35 +23,23 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<HttpStatus> create(@RequestBody @Valid User user, BindingResult bindingResult){
-//        if(bindingResult.hasErrors()){
-//            throw new ValidationException(bindingResult.getAllErrors().toString());
-//        }
         userService.createUser(user);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @GetMapping("/getByBirthDateRange")
     public ResponseEntity<List<User>> getUsers(@RequestBody @Valid DateRangeDto dateRangeDto, BindingResult bindingResult){
-//        if(bindingResult.hasErrors()){
-//            throw new ValidationException(bindingResult.getAllErrors().toString());
-//        }
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUsersByBirthRange(dateRangeDto));
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<HttpStatus> update(@RequestBody @Valid UpdateUserDto updateUserDto, @PathVariable Long id, BindingResult bindingResult){
-//        if(bindingResult.hasErrors()){
-//            throw new ValidationException(bindingResult.getAllErrors().toString());
-//        }
         userService.updateUser(id, updateUserDto);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<HttpStatus> update(@RequestBody @Valid User user, @PathVariable Long id, BindingResult bindingResult){
-//        if(bindingResult.hasErrors()){
-//            throw new ValidationException(bindingResult.getAllErrors().toString());
-//        }
         userService.updateUser(id, user);
         return new ResponseEntity<>(HttpStatus.OK);
     }
